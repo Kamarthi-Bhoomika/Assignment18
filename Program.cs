8d09353fe0820aabf82e15676bbc2efcb7c57409
unknown <kbhoomika05@gmail.com>
2023-07-28 15:15:14 +0530

    Assignment 18 Multiple Interface

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartPhone smartPhone = new SmartPhone();
+            smartPhone.Connect();
+            smartPhone.Charge(20);
+            smartPhone.Display();
+
+            Laptop laptop = new Laptop();
+            laptop.Connect();
+            laptop.Charge(50);
+            laptop.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SmartPhone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment18
+{
+    internal class SmartPhone : IConnectable, IRechargable, IDisplayable
+    {
+        int charge;
+        string info;
+        public SmartPhone()
+        {
+            charge = 0;
+            info = "Connecti";
+        }
+        public int Charge(int minutes)
+        {
+            charge += minutes;
+            return charge;
+        }
+
+        public bool Connect()
+        {
+            Console.WriteLine("Connected Successfully to SmartPhone!!");
+            return true;
+        }
+
+        public string Display()
+        {
+            Console.WriteLine("Charging Time: " + charge + " minutes");
+            return info;
+        }
+    }
+}
